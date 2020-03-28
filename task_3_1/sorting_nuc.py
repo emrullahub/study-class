5b0ad0def4476fe0c37cf1ccaf0118880a963c9f
Emrullah Yılmaz <62052339+emrullahub@users.noreply.github.com>
2020-03-29 00:07:19 +0300

    Update sorting_nuc.py

--- task_3_1/sorting_nuc.py
+++ task_3_1/sorting_nuc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 
-with open('/truba/home/emyilmaz/study-class/task_3_1/N_den_cickoy_4784_01_sorted_proper_rmdup.sam', 'r') as f:
+with open('/path/to/file.sam', 'r') as f:
 	for line in f:
 		if 'mitochondria' not in line and  'chloroplast' not in line:
 
